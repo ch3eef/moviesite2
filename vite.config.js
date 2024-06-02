74559c6d6f2253465e83e57abcbd438beb090728
ch3eef <ezeemmanuel304@gmail.com>
2024-06-02 19:51:24 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/moviesite2/",
+  // base: "/moviesite2/",
   plugins: [react()],
 })
